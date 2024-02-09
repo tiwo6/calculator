adef9f860156e6781fc6f7e3764f756d2a801776
tiwo6 <136822495+tiwo6@users.noreply.github.com>
2024-02-09 10:28:50 +0100

    Add delete function and float function

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function add(x, y) {
-    return Number(x) + Number(y);
+    return Math.round(((Number(x) + Number(y)) + Number.EPSILON) * 100) / 100;
 }
 
 function subtract(x, y) {
-    return Number(x) - Number(y);
+    return Math.round(((Number(x) - Number(y)) + Number.EPSILON) * 100) / 100;
 }
 
 function multiply(x, y) {
-    return Number(x) * Number(y);
+    return Math.round(((Number(x) * Number(y)) + Number.EPSILON) * 100) / 100;
 }
 
 function divide(x, y) {
+    if (y == 0) return "ERROR ZERO";
     return Math.round(((Number(x) / Number(y)) + Number.EPSILON) * 100) / 100;
 }
 
@@ -26,8 +27,8 @@ const operators = document.querySelector(".operators");
 const display = document.querySelector(".display");
 const equals = document.querySelector(".equals");
 const clear = document.querySelector(".clear");
-
-console.log(numbers);
+const float = document.querySelector(".float");
+const del = document.querySelector(".del");
 
 let number = null;
 let input = [];
@@ -39,50 +40,75 @@ numbers.forEach((element) => element.addEventListener("click", function(e) {
     number == null ? number = e.target.textContent : 
     number += e.target.textContent;
     display.textContent = number;
-    console.log(input);
 }))
 
 
 operators.addEventListener("click", function(e) {
-    if (number != null) {
+
+    if (input.length == 0 || input.length == 2) {
         input.push(number);
         number = null;
     }
     let operator = e.target.textContent;
-    input.push(operator);    
-        
-    if (input.length >= 3) {
+    if (input.length == 1) input.push(operator);
+
+    if (input.length == 3) {
         result = operate(input[0], input[2], input[1]);
+        display.textContent = result;
+        input = [];
         input[0] = result;
         input[1] = operator;
-        input.pop();
-        input.pop();
     }   
-
-    if (result != null) result.textContent = result;
-    console.log(input);
+    if (result == "ERROR ZERO") {
+        number = null;
+        input = [];
+        result = null;
+    }
+    float.disabled = false;
  })
+
 
  equals.addEventListener("click", function() {
     if (input.length == 1) {
         display.textContent = input[0];
     }
-    else {
+    if (input.length == 0 || input.length == 2) {
         input.push(number);
+        number = null;
+    }
+    if (input.length == 3) {
         result = operate(input[0], input[2], input[1]);
         display.textContent = result;
-        input[0] = result;
-        input.pop();
-        input.pop();
-        number = null;   
+        input = [];
+        input[0] = result; 
     }
-    console.log(input);
-
+    if (result == "ERROR ZERO") {
+        number = null;
+        input = [];
+        result = null;
+    }
+    float.disabled = false;
  })
 
 clear.addEventListener("click", function() {
     number = null;
     input = [];
     result = null;
-    display.textContent = "";
+    display.textContent = "0";
+    float.disabled = false;
+})
+
+float.addEventListener("click", function(e) {
+    if (number != null) {
+        number += e.target.textContent;
+        display.textContent = number;
+        float.disabled = true;
+    }
+})
+
+del.addEventListener("click", function() {
+    if (number != null) {
+        number = number.substring(0, number.length - 1);
+        display.textContent = number;
+    }
 })
